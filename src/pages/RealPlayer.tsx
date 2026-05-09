a91d9bf294c0c17e12e0ac8d282ddc8b495dc38b
[dyad] <horacevecora@gmail.com>
2026-05-09 06:27:19 +0100

    [dyad] تعديل رابط فيسبوك لاستخدام القيم الرقمية لضمان تشغيل الصوت. - wrote 1 file(s)

--- src/pages/RealPlayer.tsx
+++ src/pages/RealPlayer.tsx
@@ -297,9 +297,9 @@ export default function RealPlayer() {
         container.appendChild(ifr);
       } else if (isFB) {
         const ifr = document.createElement("iframe");
-        // استخدام mute=false لفيسبوك لضمان محاولة تشغيل الصوت
-        const fbMute = shouldUnmute ? "false" : "true";
-        ifr.src = `https://www.facebook.com/plugins/video.php?href=${encodeURIComponent(url)}&show_text=0&autoplay=1&mute=${fbMute}&allowfullscreen=true&adapt_to_wrapper=true`;
+        // استخدام mute=0 و autoplay=true لضمان أفضل توافق مع فيسبوك
+        const fbMute = shouldUnmute ? "0" : "1";
+        ifr.src = `https://www.facebook.com/plugins/video.php?href=${encodeURIComponent(url)}&show_text=0&autoplay=true&mute=${fbMute}&allowfullscreen=true&adapt_to_wrapper=true`;
         ifr.style.width = "100%"; 
         ifr.style.height = "100%"; 
         ifr.style.border = "none";
