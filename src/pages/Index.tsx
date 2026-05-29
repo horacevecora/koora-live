179f42b26589e133da0aa1a7bb7137bc01fa0582
[dyad] <horacevecora@gmail.com>
2026-05-29 20:18:33 +0100

    [dyad] تنظيف الأكواد الإعلانية من الملفات المصدرية - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -64,14 +64,6 @@ const Index = () => {
         <meta property="og:image" content="/favicon.svg" />
         <meta property="og:type" content="website" />
         
-        {/* كود التحقق الأساسي */}
-        <script src="https://alwingulla.com/88/p.js?management=892345" type="text/javascript"></script>
-        
-        {/* كود Onclick / Popunder */}
-        <script type="text/javascript">
-          {`(function(s){s.dataset.zone='10984911',s.src='https://al5sm.com/tag.min.js'})([document.documentElement,document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}
-        </script>
-        
         <script type="application/ld+json">
           {JSON.stringify(schemaData)}
         </script>
@@ -108,7 +100,6 @@ const Index = () => {
           </button>
         </div>
 
-        {/* اسم الموقع قابل للضغط */}
         <Link to="/" className="flex flex-col items-end group transition-opacity hover:opacity-80">
           <div className="flex items-center gap-1 text-[#00e676] font-black text-2xl italic tracking-tighter leading-none">
             <span>Koora</span>
